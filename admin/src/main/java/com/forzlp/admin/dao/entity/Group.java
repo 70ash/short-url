426b0ee04c3134573cd6919d57c387b4049df36f
70ash <zlianpei@gmail.com>
2024-02-20 19:36:45 +0800

    实体类

---
+++ admin/src/main/java/com/forzlp/admin/dao/entity/Group.java
@@ -0,0 +1,48 @@
+package com.forzlp.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Author 70ash
+ * @Date 2024/2/20 16:37
+ * @Description:
+ */
+@Data
+public class Group extends BaseDO implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(type = IdType.AUTO)
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 创建分组用户名
+     */
+    private String username;
+
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
+
+
+
+    public Group() {}
+}
