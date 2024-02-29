9274a6f65cabd12cad8b2cec361a2a3e6782fc90
70ash <zlianpei@gmail.com>
2024-02-29 22:06:52 +0800

    短链接分组展示

---
+++ admin/src/main/java/com/example/demo/dto/resp/ListGroupRespDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto.resp;
+
+import lombok.Data;
+
+/**
+ * @Author 70ash
+ * @Date 2024/2/29 21:40
+ * @Description:
+ */
+@Data
+public class ListGroupRespDTO {
+    private String name;
+}
