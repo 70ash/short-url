167ea108e2ebf3716c3b93e67ee4f56e1db8a573
70ash <zlianpei@gmail.com>
2024-02-29 22:16:40 +0800

    某一分组下短链接展示Resp

---
+++ admin/src/main/java/com/example/demo/dto/resp/ListLinkRespDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto.resp;
+
+import lombok.Data;
+
+/**
+ * @Author 70ash
+ * @Date 2024/2/29 22:05
+ * @Description:
+ */
+@Data
+public class ListLinkRespDTO {
+    private String fullShortUrl;
+
+}
