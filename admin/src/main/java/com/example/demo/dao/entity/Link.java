87fe390c74a0dd683fe84907f7860df05c324909
70ash <zlianpei@gmail.com>
2024-03-01 17:50:58 +0800

    实体类，对应于数据库中的t_link

---
+++ admin/src/main/java/com/example/demo/dao/entity/Link.java
@@ -0,0 +1,70 @@
+package com.example.demo.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Author 70ash
+ * @Date 2024/2/29 22:04
+ * @Description:
+ */
+@Data
+public class Link extends BaseDO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(type = IdType.AUTO)
+    /**
+     * id     primary key
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUrl;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 点击量
+     */
+    private Integer clickNum;
+
+    /**
+     * 启用状态，0：启用，1：启用
+     */
+    private int enableStatus;
+
+    /**
+     * 有效时间，0：永久有效，1：自定义
+     */
+    private int validType;
+
+    /**
+     * 有效时间
+     */
+    private Date validTime;
+}
