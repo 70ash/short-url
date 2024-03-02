29bd831682af76aa32ba14e1bebb8ede7a28f10d
70ash <zlianpei@gmail.com>
2024-03-02 15:54:52 +0800

    短链接分组模块

--- admin/src/main/java/com/example/demo/controller/GroupController.java
+++ admin/src/main/java/com/example/demo/controller/GroupController.java
@@ -2,12 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.common.convention.result.Result;
 import com.example.demo.common.convention.result.Results;
+import com.example.demo.dto.req.UpdateGroupReqDTO;
+import com.example.demo.dto.resp.ListGroupRespDTO;
 import com.example.demo.service.GroupService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Author 70ash
@@ -19,9 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class GroupController {
     private final GroupService groupService;
+    // 新增短链接分组
     @PostMapping("/save")
-    public Result saveGroup(@RequestParam("groupName") String groupName) {
+    public Result<Void> saveGroup(@RequestParam("groupName") String groupName) {
         groupService.saveGroup(groupName);
         return Results.success();
+    }
+    // 修改短链接分组
+    @PostMapping()
+    public Result<Void> updateGroup(@RequestBody UpdateGroupReqDTO requestParam) {
+        groupService.updateGroup(requestParam);
+        return Results.success();
+    }
+    // 删除短链接分组
+    @DeleteMapping()
+    public Result<Void> updateGroup(String gid) {
+        groupService.updateGroup(gid);
+        return Results.success();
+    }
+    // 查看短链接分组
+    @GetMapping("/list")
+    public Result<List<ListGroupRespDTO>> listGroup() {
+        List<ListGroupRespDTO> list = groupService.listGroup();
+        return Results.success(list);
     }
 }
