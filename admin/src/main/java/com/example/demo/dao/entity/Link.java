b39458109fca3e90fa8cb196f3039aae23520e89
70ash <zlianpei@gmail.com>
2024-03-02 14:14:49 +0800

    添加注解@Builder

--- admin/src/main/java/com/example/demo/dao/entity/Link.java
+++ admin/src/main/java/com/example/demo/dao/entity/Link.java
@@ -2,6 +2,7 @@ package com.example.demo.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -13,6 +14,7 @@ import java.util.Date;
  * @Description:
  */
 @Data
+@Builder
 public class Link extends BaseDO implements Serializable {
 
     private static final long serialVersionUID = 1L;
