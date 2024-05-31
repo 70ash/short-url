e199919476fbe384ec4c384d8dd19832af8e69e6
70ash <zlianpei@gmail.com>
2024-05-31 10:59:52 +0800

    refactor: 用户登录状态保存时间修改

--- admin/src/main/java/com/example/demo/common/constant/RedisConstant.java
+++ admin/src/main/java/com/example/demo/common/constant/RedisConstant.java
@@ -10,5 +10,5 @@ public class RedisConstant {
     public static final String USER_LOGIN_KEY = "login:";
     public static final String USER_TOKEN_KEY = "login:token:";
 
-    public static final Long USER_TOKEN_TIME = 30L;
+    public static final Long USER_TOKEN_TIME = 100L;
 }
