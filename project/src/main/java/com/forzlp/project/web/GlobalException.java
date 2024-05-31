92b1bc8d06a071fad253ce672283b4acd1c80286
70ash <zlianpei@gmail.com>
2024-05-31 11:03:35 +0800

    refactor: 石山重构

--- project/src/main/java/com/forzlp/project/web/GlobalException.java
+++ project/src/main/java/com/forzlp/project/web/GlobalException.java
@@ -2,10 +2,10 @@ package com.forzlp.project.web;
 
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.StrUtil;
-import com.forzlp.common.common.convention.errorcode.BaseErrorCode;
-import com.forzlp.common.common.convention.excetion.AbstractException;
-import com.forzlp.common.common.convention.result.Result;
-import com.forzlp.common.common.convention.result.Results;
+import com.forzlp.project.common.convention.errorcode.BaseErrorCode;
+import com.forzlp.project.common.convention.excetion.AbstractException;
+import com.forzlp.project.common.convention.result.Result;
+import com.forzlp.project.common.convention.result.Results;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
